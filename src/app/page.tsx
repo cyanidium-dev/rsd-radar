83d91af2778d810138b466df24f9f0c8b8b94f5a
Andrii Shtohryn <andriishtohryn@gmail.com>
2025-03-03 23:46:55 +0200

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const defaultTitle = "RSD Radar | Безпека доріг";
+  const description =
+    "RSD Radar — інноваційні рішення для безпеки доріг. Дізнайся про наші радари, переваги та напрямки використання.";
+  const keywords = [
+    "RSD Radar",
+    "безпека доріг",
+    "радар для доріг",
+    "інноваційні радари",
+  ];
+
+  return {
+    metadataBase: new URL(baseUrl),
+    keywords: keywords,
+    title: {
+      default: defaultTitle,
+      template: `%s | RSD Radar`,
+    },
+    description: description,
+    openGraph: {
+      type: "website",
+      url: baseUrl,
+      title: defaultTitle,
+      description: description,
+    },
+  };
+}
+
 import Advantages from "@/modules/Advantages/Advantages";
 import DirectionsOfUse from "@/modules/DirectionsOfUse/DirectionsOfUse";
 import InnovativeRoadSigns from "@/modules/InnovativeRoadSigns/InnovativeRoadSigns";
