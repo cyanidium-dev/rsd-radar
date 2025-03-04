077aa75fdea5d0df88f90a1ead537f34d2b22e22
Andrii Shtohryn <andriishtohryn@gmail.com>
2025-03-04 18:34:05 +0200

    update meta3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,43 @@ import "./globals.css";
 import Header from "@/modules/Header/Header";
 import Footer from "@/modules/Footer/Footer";
 import { Montserrat, Raleway } from "next/font/google";
+export async function generateMetadata(): Promise<Metadata> {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL || "https://rsd-radar.vercel.app";
+  const defaultTitle = "RSD Radar | Безпека доріг";
+  const description =
+    "RSD Radar — інноваційні рішення для безпеки доріг. Дізнайся про наші радари, переваги та напрямки використання.";
+  const keywords = [
+    "RSD Radar",
+    "безпека доріг",
+    "радар для доріг",
+    "інноваційні радари",
+  ];
 
-export const metadata: Metadata = {
-  title: "RSD Radar",
-  description: "Кращий іноваційний партнер для безпеки доріг у вашій громаді.",
-};
+  return {
+    metadataBase: new URL(baseUrl),
+    keywords: keywords,
+    title: {
+      default: defaultTitle,
+      template: `%s | RSD Radar`,
+    },
+    description: description,
+    openGraph: {
+      type: "website",
+      url: baseUrl,
+      title: defaultTitle,
+      description: description,
+      images: [
+        {
+          url: "../../public/images/radar-desk.png",
+          width: 1200,
+          height: 630,
+          alt: "RSD Radar Logo",
+        },
+      ],
+    },
+  };
+}
 
 const montserrat = Montserrat({
   subsets: ["latin"],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import Advantages from "@/modules/Advantages/Advantages";
 import DirectionsOfUse from "@/modules/DirectionsOfUse/DirectionsOfUse";
 import InnovativeRoadSigns from "@/modules/InnovativeRoadSigns/InnovativeRoadSigns";
@@ -6,42 +5,6 @@ import RadarsModels from "@/modules/RadarsModels/RadarsModels";
 import Hero from "@/modules/Hero/Hero";
 import TopContactsSection from "@/modules/Contacts/TopContactsSection";
 import BotContactsSection from "@/modules/Contacts/BotContactsSection";
-export async function generateMetadata(): Promise<Metadata> {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
-  const defaultTitle = "RSD Radar | Безпека доріг";
-  const description =
-    "RSD Radar — інноваційні рішення для безпеки доріг. Дізнайся про наші радари, переваги та напрямки використання.";
-  const keywords = [
-    "RSD Radar",
-    "безпека доріг",
-    "радар для доріг",
-    "інноваційні радари",
-  ];
-
-  return {
-    metadataBase: new URL(baseUrl),
-    keywords: keywords,
-    title: {
-      default: defaultTitle,
-      template: `%s | RSD Radar`,
-    },
-    description: description,
-    openGraph: {
-      type: "website",
-      url: baseUrl,
-      title: defaultTitle,
-      description: description,
-      images: [
-        {
-          url: "/images/radar-desk.png",
-          width: 1200,
-          height: 630,
-          alt: "RSD Radar",
-        },
-      ],
-    },
-  };
-}
 
 export default function Home() {
   return (
